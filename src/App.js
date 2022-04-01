befcc65f00389209b9d48c571523d9211dc855d3
Dongi <chaedonggi@gmail.com>
2022-04-02 01:01:05 +0900

    React Router
    When installing React Router. Please use the command below.
    
    npm i react-router-dom@5.3.0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return null;
-}
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
-export default App;
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/hello">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
